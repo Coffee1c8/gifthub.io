5f1ee6aae7314b005b1fd491a02382b57c18fe28
Coffee1c8 <andreiiacob314@gmail.com>
2025-01-22 16:31:40 +0200

    Add files via upload

---
+++ 18th.CMD.dat.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18th.CMD.dat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("By Coffee");
+            Console.WriteLine("du te la munca");
+            Console.ReadKey();
+        }
+    }
+}
